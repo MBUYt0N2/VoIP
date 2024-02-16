35a73c390e892d2b4e9196e23aead0f2f6a0b1cb
MBUYT0N <shusrith03@gmail.com>
2024-02-16 12:39:51 +0530

    nyet

--- client.py
+++ client.py
@@ -1,39 +1,32 @@
-import pyaudio
 import socket
 import wave
 import threading
 import random
+import sounddevice as sd
+import numpy as np
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = "10.20.202.95" 
 port = 9999  
-p = pyaudio.PyAudio()
 frames = []
 
-
 def send_audio():
-    stream = p.open(
-        format=pyaudio.paInt16,
-        channels=1,
-        rate=44100,
-        input=True,
-        frames_per_buffer=1024,
-    )
-
+    samplerate = 44100
+    duration = 5  
     print("Recording...")
-    for i in range(0, int(44100 / 1024 * 5)): 
-        data = stream.read(1024)
+    myrecording = sd.rec(int(samplerate * duration), samplerate=samplerate,
+                         channels=1, dtype='int16')
+    sd.wait()
+
+    print(myrecording.shape)
+
+    for i in range(0, len(myrecording)): 
+        data = myrecording[i].tobytes()
         s.sendall(data)
 
     print("Recording finished.")
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-
-# Save the recording as a wave file
 def receive_audio():
     while True:
         data = s.recv(1024)
@@ -44,13 +37,12 @@ def receive_audio():
     x = random.randint(0, 100000)
     wf = wave.open(f"output{x}.wav", "wb")
     wf.setnchannels(1)
-    wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+    wf.setsampwidth(2)  
     wf.setframerate(44100)
     wf.writeframes(b"".join(frames))
     wf.close()
 
     print("Output saved to output.wav.")
-
 
 try:
     s.connect((host, port))
