c125448fc53dea4803e4a5f6bca0841dfd85d012
MBUYT0N <shusrith03@gmail.com>
2024-02-16 13:37:33 +0530

    ;)

--- zdynamicip.py
+++ zdynamicip.py
@@ -4,6 +4,5 @@ def get_server_ip():
     output = subprocess.check_output("ifconfig | grep inet", shell=True).decode()
     lines = output.split("\n")
     serverip = lines[5].strip().split(" ")[1]
-
     return serverip
 
