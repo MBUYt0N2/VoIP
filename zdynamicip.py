6cffd85141ced12358e48199d034d7db55c44c13
MBUYt0n <shusrith03@gmail.com>
2024-02-21 04:31:07 +0530

    dynamic ip

--- zdynamicip.py
+++ zdynamicip.py
@@ -2,10 +2,15 @@ import subprocess
 
 
 def get_server_ip():
-    output = subprocess.check_output("ifconfig | grep inet", shell=True).decode()
-    lines = output.split("\n")
+    output = subprocess.check_output("ifconfig | grep 192.", shell=True).decode()
+    if output is None:
+        output = subprocess.check_output("ifconfig | grep 10.", shell=True).decode()
+    lines = output.split(" ")
     for i in lines:
         if "192." in i or "10." in i:
-            serverip = i.strip().split(" ")[1]
+            serverip = i.strip()
             break
     return serverip
+
+
+print(get_server_ip())
