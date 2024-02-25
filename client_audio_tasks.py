646c0f06f15c89b996d754c9da67a2cbb9882bd1
MBUYt0n <shusrith03@gmail.com>
2024-02-25 20:00:01 +0530

    ..

--- client_audio_tasks.py
+++ client_audio_tasks.py
@@ -1,6 +1,6 @@
 import sounddevice as sd
 import numpy as np
-
+import wave
 
 frames = []
 
@@ -22,7 +22,24 @@ def send_audio(s):
     print("Recording finished.")
 
 
+# def receive_audio(s):
+#     while True:
+#         data = s.recv(1024)
+#         if b"end" in data:
+#             break
+#         data_len = len(data)
+#         if data_len % 2 != 0:
+#             data += s.recv(1)
+#         audio_array = np.frombuffer(data, dtype=np.int16)
+#         print(audio_array[:5])
+#         sd.play(audio_array, samplerate=44100)
+#         sd.wait()
+#         print("playing")
+#     print("done")
+
+
 def receive_audio(s):
+    audio_data = b""
     while True:
         data = s.recv(1024)
         if b"end" in data:
@@ -30,9 +47,10 @@ def receive_audio(s):
         data_len = len(data)
         if data_len % 2 != 0:
             data += s.recv(1)
-        audio_array = np.frombuffer(data, dtype=np.int16)
-        print(audio_array[:5])
-        sd.play(audio_array, samplerate=44100)
-        sd.wait()
-        print("playing")
+        audio_data += data
     print("done")
+    with wave.open("output.wav", "wb") as wav_file:
+        wav_file.setnchannels(1)
+        wav_file.setsampwidth(2)  # 2 bytes for int16
+        wav_file.setframerate(44100)
+        wav_file.writeframes(audio_data)
