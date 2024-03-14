c0bfbc7e6cee458c1cee1c54413cdc28790b2cb8
MBUYt0n <shusrith03@gmail.com>
2024-03-14 07:22:24 +0530

    .

--- udp/client_audio_tasks.py
+++ udp/client_audio_tasks.py
@@ -9,7 +9,7 @@ frames = []
 audio_buffer = queue.Queue()
 last_received_audio = None
 sending = True
-pauser = False
+muter = False
 
 
 class StreamEnd(Exception):
@@ -22,24 +22,24 @@ def end_call(s):
     s.close()
 
 
-def pause():
-    global pauser
-    pauser = True
+def mute():
+    global muter
+    muter = True if muter == False else False
 
 
 def send_audio(s, host, port):
     global sending
-    global pauser
+    global muter
     samplerate = 48000
     print("Recording...")
 
-    def callback(indata, frames, time, status):
+    def callback(indata, frames, time_info, status):
         global sending
         data = indata.astype(np.float32)
         encoded_audio = g711.encode_ulaw(data)
-        if pauser:
-            print("Pausing")
-            raise sd.CallbackStop
+        if muter:
+            print("muted")
+            time.sleep(0.1)
         elif sending:
             s.sendto(encoded_audio, (host, port))
         elif not sending:
@@ -58,7 +58,6 @@ def receive_audio(s, host, port):
     s1.bind(("", 9000))
     global audio_buffer
     global last_received_audio
-    global pauser
     samplerate = 48000
     dtype = "float32"
     channels = 1
@@ -68,7 +67,7 @@ def receive_audio(s, host, port):
 
     stream.start()
 
-    while sending and not pauser:
+    while sending:
         data, addr = s1.recvfrom(16384)
         try:
             decoded_audio = g711.decode_ulaw(data)
